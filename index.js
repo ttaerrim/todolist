9adc9f40860d3f9b52733ccf3c1bd73725bc33fd
Lee <ltr0121@naver.com>
2020-08-14 23:10:30 +0900

    alter Clock form 00:00:00

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+/*
 const title = document.querySelector('#title');
 
 const CLICKED_CLASS = 'clicked';
 
 function handleClick() {
-    /*
+
     const hasClass = title.classList.contains(CLICKED_CLASS);
     if (!hasClass) {
         title.classList.add(CLICKED_CLASS);
@@ -11,7 +12,7 @@ function handleClick() {
         title.classList.remove(CLICKED_CLASS);
     }
     toggle이 같은 기능을 함
-    */
+
     title.classList.toggle(CLICKED_CLASS);
 }
 function init() {
@@ -19,3 +20,4 @@ function init() {
 }
 
 init();
+*/
